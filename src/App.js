cc4983e55bcec5314a3e729017426d04590b059b
jeonwanjin <jwj1212121@gmail.com>
2023-11-02 17:12:32 +0900

    1102

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Main from "./component/main"
 import Section1_subPage from "./page/section1_subPage";
 import Section2_subPage from "./page/section2_subPage";
 import Section3_subPage from "./page/section3_subPage";
+import Section4_announcement1 from "./page/section4_announcement1";
+import Section4_announcement2 from "./page/section4_announcement2";
+import Section5_subPage from "./page/section5_subPage";
 
 function App() {
   return(
@@ -21,10 +24,10 @@ function App() {
           <Route path="/Guide" element={<><Header/><Section1_subPage/></>}/>
           <Route path="/Program" element={<><Header/><Section2_subPage/></>}/>
           <Route path="/Event" element={<><Header/><Section3_subPage/></>}/>
-          <Route path="/NoticeList" element={<></>}/>
-          <Route path="/NoticeWrite" element={<></>}/>
+          <Route path="/NoticeList" element={<><Header/><Section4_announcement1 /></>}/>
+          <Route path="/NoticeWrite" element={<><Header/><Section4_announcement2 /></>}/>
           <Route path="/FAQ" element={<></>}/>
-          <Route path="/Gallery" element={<></>}/>
+          <Route path="/Gallery" element={<><Header/><Section5_subPage/></>}/>
           <Route path="/Login" element={<></>}/>
           <Route path="/Join" element={<></>}/>
         </Routes>
