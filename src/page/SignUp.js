f94ceabed063a9a4eda3deba4c9444e7d60e9de6
jeonwanjin <jwj1212121@gmail.com>
2023-11-06 11:46:19 +0900

    1106

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -26,26 +26,75 @@ const SignUp = () => {
                             <label>수집 및 이용 동의</label>
                         </div>
                         <div className='inputBox'>
+
                             <div className='inputDiv'>
                                 <input type='text'></input>
                             </div>
+
                             <div className='inputDiv'>
                                 <input type='text'></input><button>중복확인</button>
-                                <p>*이용자ID의 길이는 8 ~ 16자 입니다.</p>
+                                <p className='redTxt'>*이용자ID의 길이는 8 ~ 16자 입니다.</p>
                             </div>
+                            
                             <div className='inputDiv'>
                                 <input type='text' placeholder='8~16자 영문대소문,숫자,특수문'></input>
                             </div>
+
                             <div className='inputDiv'>
                                 <input type='text'></input>
                             </div>
-                            <div className='inputDiv'>
-                                <input type="radio" name="y"></input><span>양력</span>
-                                <input type="radio" name="y"></input><span>음력</span>
+
+                            <div className='birth flex'>
+                                <div className='inputDiv'>
+                                    <input type="radio" name="y"></input><span>양력</span>
+                                    <input type="radio" name="y"></input><span>음력</span>
+                                </div>
+                                <div className='selectDiv'>
+                                    <select>
+                                        <option selected>1999</option>
+                                    </select>
+                                    <select>
+                                        <option selected>10</option>
+                                    </select>
+                                    <select>
+                                        <option selected>13</option>
+                                    </select>
+                                </div>
                             </div>
 
+                           <div className='tel flex2'>
+                                <select>
+                                    <option selected>010</option>
+                                </select>
+                                <input className='telInput' type='text'></input>
+                                <input className='telInput' type='text'></input>
+                                <button>인증하기</button>
+                           </div>
+
+                           <div className='eMail flex2'>
+                                <input className='mailInput' type='text'></input>
+                                <div className='middle'>@</div>
+                                <input className='mailInput' type='text'></input>
+                                <select>
+                                    <option selected>직접입력</option>
+                                </select>
+                                <button>인증발송</button>
+                           </div>
+                           <p className='redTxt'>*메일을 인증해주세요.</p>
+
+                           <div className='check flex2'>
+                                <input type="checkbox"></input><p>&#91;필수&#93; 이용약관 동의 보기</p>
+                           </div>
+                           <div className='flex2'>
+                                <input type="checkbox"></input><p>&#91;필수&#93;  개인정보 수집 및 이용 동의 보기</p>
+                           </div>
+                           <div className='flex2'>
+                                <input type="checkbox"></input><p>&#91;선택&#93; 선택정보 수집 및 이용 동의 보기 </p>
+                           </div>
                         </div>
-                       
+                    </div>
+                    <div className='SignUpBtn'>
+                        <button>가입하기</button>
                     </div>
                 </div>
         </section>
