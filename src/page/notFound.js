e6bc70a5cb6e6d2f2d33fb9019f7f0aa5f8fdbbf
CuteSungMin <skje8162@gamil.com>
2023-11-08 17:17:41 +0900

    진짜 최종의 최종임

--- src/page/notFound.js
+++ src/page/notFound.js
@@ -1,8 +1,10 @@
+import '../pageCss/notFound.css'
+
 const NotFound = ()=>{
     return(
         <section className="NotFound">
             <h1>404</h1>
-            <div>주소를 잘못 입력하심</div>
+            <div>소인의 소홀한 실수로 말미암아 주소를 올바로 기록하지 못하였나이다.</div>
         </section>
     );
 }
